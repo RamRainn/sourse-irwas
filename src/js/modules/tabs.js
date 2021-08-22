f9e07e20d1665e53123616d37be0e9c3256edb53
RamRain <ramrain.411@gmail.com>
2021-08-22 14:10:23 +0500

    add calc modal and timer

--- src/js/modules/tabs.js
+++ src/js/modules/tabs.js
@@ -1,44 +1,39 @@
-const tabs = (headerSelector, tabSelector, contentSelector, activeClass) => {
-
+const tabs = (headerSelector, tabSelector, contentSelector, activeClass, display = 'block') => {
     const header = document.querySelector(headerSelector),
           tab = document.querySelectorAll(tabSelector),
           content = document.querySelectorAll(contentSelector);
 
-
-        function hideTabContent() {
-            content.forEach(item => {
-                item.style.display = 'none'; // скрытие
-            });
-
-            tab.forEach(item => {
-                item.classList.remove(activeClass); // убираем класс активности
-            });
-        }
-
-        function showTabContent(i = 0) {
-            content[i].style.display = 'block'; // показывается 
-            tab[i].classList.add(activeClass); // добавляем класс активности
-        }
-
-        hideTabContent();
-        showTabContent();
-
-        header.addEventListener('click', (e) => {
-
-            const target = e.target;
-            if (target && 
-                (target.classList.contains(tabSelector.replace(/\./, "")) ||
-             target.parentNode.classList.contains(tabSelector.replace(/\./, "")))) {
-                tab.forEach((item, i) => {
-                    if (target == item || target.parentNode == item) {
-                        hideTabContent();
-                        showTabContent(i);
-                    }
-                });
-            }
+    function hideTabContent() {
+        content.forEach(item => {
+            item.style.display = 'none';
         });
 
+        tab.forEach(item => {
+            item.classList.remove(activeClass);
+        });
+    }
 
+    function showTabContent(i = 0) {
+        content[i].style.display = display;
+        tab[i].classList.add(activeClass);
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    header.addEventListener('click', (e) => {
+        const target = e.target;
+        if (target &&
+            (target.classList.contains(tabSelector.replace(/\./, "")) || 
+        target.parentNode.classList.contains(tabSelector.replace(/\./, "")))) {
+            tab.forEach((item, i) => {
+                if (target == item || target.parentNode == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
 };
 
 export default tabs;
